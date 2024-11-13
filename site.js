3b1bf465ec58bb27e7c6472524981e048345af73
salimalitarikz <salimalitarikz@gmail.com>
2024-11-13 05:18:31 +0300

    d2-u4-v1

--- site.js
+++ site.js
@@ -1,1 +1,25 @@
 document.querySelector("#hero_video").playbackRate = 1;
+
+
+// Initial state
+var scrollPos = 100;
+// adding scroll event
+window.addEventListener('scroll', function(){
+  // detects new state and compares it with the new one
+
+  if (window.scrollY>300) {
+    document.getElementById("nav").classList.add("nav_passive");
+    if ((document.body.getBoundingClientRect()).top < scrollPos)
+        document.getElementById("nav").classList.remove("nav_active");
+    
+        else
+        document.getElementById("nav").classList.add("nav_active");
+        // saves the new position for iteration.
+        scrollPos = (document.body.getBoundingClientRect()).top;
+  }else{
+    document.getElementById("nav").classList.remove("nav_active");
+    document.getElementById("nav").classList.remove("nav_passive");
+  }
+  
+});
+
